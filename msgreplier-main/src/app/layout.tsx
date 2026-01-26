95318470503351c3f81571c0dd4d96719090f98d
asrithreddy755 <sattiasrithreddy@gmail.com>
2026-01-26 19:47:48 +0530

    versel final adds

--- msgreplier-main/src/app/layout.tsx
+++ msgreplier-main/src/app/layout.tsx
@@ -67,12 +67,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8011470049569108"
-          crossOrigin="anonymous"
-          strategy="beforeInteractive"
-        />
+        {process.env.NODE_ENV === "production" && (
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8011470049569108"
+            crossOrigin="anonymous"
+          ></script>
+        )}
       </head>
       <body className="font-body antialiased">
         <Script
