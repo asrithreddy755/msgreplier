93f1ba98c6e86d65f4804ff9476d7e26c95c47de
asrithreddy755 <sattiasrithreddy@gmail.com>
2026-03-01 13:57:55 +0530

    fixed the deploy error

--- msgreplier-main/next.config.mjs
+++ msgreplier-main/next.config.mjs
@@ -37,6 +37,31 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: /react/,
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
